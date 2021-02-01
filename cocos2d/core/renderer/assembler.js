4cd301cce9fb0c7d686b6abd83fda2d5d0b7cc42
王安康 <wangankang.ankang@bytedance.com>
2021-02-01 13:38:41 +0800

    feat: 新增assembler的注释

--- cocos2d/core/renderer/assembler.js
+++ cocos2d/core/renderer/assembler.js
@@ -8,26 +8,31 @@ export default class Assembler {
     init (renderComp) {
         this._renderComp = renderComp;
     }
-    
+
     updateRenderData (comp) {
     }
 
     fillBuffers (comp, renderer) {
     }
-    
+
     getVfmt () {
         return vfmtPosUvColor;
     }
 }
 
 
+// 将 assembler Clazz 绑定到 render 组件的静态参数内
 Assembler.register = function (renderCompCtor, assembler) {
     renderCompCtor.__assembler__ = assembler;
 };
 
+// 初始化, 传入组件实例
 Assembler.init = function (renderComp) {
+    // render 组件Clazz
     let renderCompCtor = renderComp.constructor;
+    // 找到组件中静态参数__assembler__ 绑定的自定义渲染Clazz
     let assemblerCtor =  renderCompCtor.__assembler__;
+    // 递归去超类中找到绑定过的assembler
     while (!assemblerCtor) {
         renderCompCtor = renderCompCtor.$super;
         if (!renderCompCtor) {
@@ -36,13 +41,20 @@ Assembler.init = function (renderComp) {
         }
         assemblerCtor =  renderCompCtor.__assembler__;
     }
+    // 如果assembler中有自定义获取构造器的function, 则使用自定义的构造器
     if (assemblerCtor.getConstructor) {
         assemblerCtor = assemblerCtor.getConstructor(renderComp);
     }
-    
+
+    // 组件中没有初始化assembler, 或者组件中assembler不是刚刚找到的assembler类, 则重新实例化
     if (!renderComp._assembler || renderComp._assembler.constructor !== assemblerCtor) {
+        // 从assembler对象池中复用已经实例化好的, 如果没有找到则执行new操作
         let assembler = assemblerPool.get(assemblerCtor);
+
+        // 双向绑定
+        // 执行初始化操作, 默认将组件实例放入assembler实例的_renderComp成员变量中
         assembler.init(renderComp);
+        // 将assembler实例放入组件历史_assembler实例中
         renderComp._assembler = assembler;
     }
 };
